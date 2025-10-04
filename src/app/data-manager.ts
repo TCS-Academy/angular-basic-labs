b782691ea7f4eddedd12b2c6f74094697c55c37d
st119848 <40877288+st119848@users.noreply.github.com>
2025-10-04 11:33:29 +0700

    use map in getPost

--- src/app/data-manager.ts
+++ src/app/data-manager.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // 2.1 นำเข้า HttpClient
 import { Observable } from 'rxjs'; // 2.2 นำเข้า Observable
+import { map } from 'rxjs/operators'; // 1.1 นำเข้า map operator
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,18 @@ export class DataManager {
   constructor(private http: HttpClient) { } // 2.3 DI เพื่อนำเข้า HttpClient
 
   getPosts(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl)
+      .pipe( // 1.2 ใช้ pipe เพื่อต่อ Operator
+        map(posts => {
+          // 1.3 ใช้ map เพื่อแปลงข้อมูลแต่ละ Post ใน Array
+          return posts.map(post => ({
+            id: post.id,
+            title: post.title // เลือกเฉพาะ id และ title
+          }));
+        })
+      );
   }
+
 
   getCounter(): number {
     return this.counterValue;
